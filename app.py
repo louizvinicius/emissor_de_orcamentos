14f23ae14d7d7deaf1afa6396cf871f26f9c1492
louizvinicius <luizvinicius1.lva24@gmail.com>
2024-08-12 17:14:12 -0300

    Campos Adicionado

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def salvar_dados():
         nome_arquivo += ".docx"
     
     caminho_arquivo = filedialog.asksaveasfilename(defaultextension=".docx", 
-                                                   filetypes=[("Word Files", "*.docx")],
-                                                   initialfile=nome_arquivo)
+        filetypes=[("Word Files", "*.docx")],
+            initialfile=nome_arquivo)
     
     if not caminho_arquivo:
         return
@@ -34,7 +34,8 @@ def salvar_dados():
             "DDDD": opcao_selecionada_area.get(),
             "EEEE": entry_entrega1.get(),
             "FFFF": entry_entrega2.get(),
-            "GGGG": entry_vendedor.get()
+            "GGGG": entry_vendedor.get(),
+            "HHHH": entry_m2.get()
         }
 
         for paragrafo in documento.paragraphs:
@@ -66,23 +67,25 @@ root = tk.Tk()
 root.title("Orçamento Alves Gramas")
 
 # Criando os widgets
+label_m2 = tk.Label(root, text="M²")
+label_m2.grid(row=0, column=0, padx=10, pady=5)
+entry_m2 = tk.Entry(root)
+entry_m2.grid(row=0, column=1, padx=10, pady=5)
+
 label_grama = tk.Label(root, text="Grama:")
-label_grama.grid(row=0, column=0, padx=10, pady=5)
+label_grama.grid(row=0, column=2, padx=10, pady=5)
 opcoes_grama = ["Selecione a grama", "Esmeralda", "Bermuda", "São Carlos", "Santo Agostinho", "Batatais", "Coreana"]
 opcao_selecionada = tk.StringVar()
 opcao_selecionada.set(opcoes_grama[0])
 menu_opcoes_grama = tk.OptionMenu(root, opcao_selecionada, *opcoes_grama)
-menu_opcoes_grama.grid(row=0, column=1, padx=10, pady=5)
+menu_opcoes_grama.grid(row=0, column=3, padx=10, pady=5)
+
 
 label_preco = tk.Label(root, text="Preço R$:")
-label_preco.grid(row=0, column=2, padx=10, pady=5)
+label_preco.grid(row=1, column=0, padx=10, pady=5)
 entry_preco = tk.Entry(root)
-entry_preco.grid(row=0, column=3, padx=10, pady=5)
+entry_preco.grid(row=1, column=1, padx=10, pady=5)
 
-label_cidade = tk.Label(root, text="Cidade-Estado:")
-label_cidade.grid(row=1, column=0, padx=10, pady=5)
-entry_cidade = tk.Entry(root)
-entry_cidade.grid(row=1, column=1, padx=10, pady=5)
 
 label_area = tk.Label(root, text="Área:")
 label_area.grid(row=1, column=2, padx=10, pady=5)
@@ -92,32 +95,37 @@ opcao_selecionada_area.set(opcoes_area[0])
 menu_opcoes_area = tk.OptionMenu(root, opcao_selecionada_area, *opcoes_area)
 menu_opcoes_area.grid(row=1, column=3, padx=10, pady=5)
 
+label_cidade = tk.Label(root, text="Cidade-Estado:")
+label_cidade.grid(row=2, column=0, padx=10, pady=5)
+entry_cidade = tk.Entry(root)
+entry_cidade.grid(row=2, column=1, padx=10, pady=5)
+
 label_entrega1 = tk.Label(root, text="Dia de entrega(min):")
-label_entrega1.grid(row=4, column=0, padx=10, pady=5)
+label_entrega1.grid(row=2, column=2, padx=10, pady=5)
 entry_entrega1 = tk.Entry(root)
-entry_entrega1.grid(row=4, column=1, padx=10, pady=5)
+entry_entrega1.grid(row=2, column=3, padx=10, pady=5)
 
 label_entrega2 = tk.Label(root, text="Dia de entrega(max):")
-label_entrega2.grid(row=4, column=2, padx=10, pady=5)
+label_entrega2.grid(row=3, column=0, padx=10, pady=5)
 entry_entrega2 = tk.Entry(root)
-entry_entrega2.grid(row=4, column=3, padx=10, pady=5)
+entry_entrega2.grid(row=3, column=1, padx=10, pady=5)
 
 label_vendedor = tk.Label(root, text="Vendedor:")
-label_vendedor.grid(row=5, column=0, padx=10, pady=5)
+label_vendedor.grid(row=4, column=0, padx=10, pady=5)
 entry_vendedor = tk.Entry(root)
-entry_vendedor.grid(row=5, column=1, padx=10, pady=5)
+entry_vendedor.grid(row=4, column=1, padx=10, pady=5)
 
 # Adiciona a entrada para o nome do arquivo
 label_nome_arquivo = tk.Label(root, text="Nome do Arquivo:")
-label_nome_arquivo.grid(row=5, column=2, padx=10, pady=5)
+label_nome_arquivo.grid(row=4, column=2, padx=10, pady=5)
 entry_nome_arquivo = tk.Entry(root)
-entry_nome_arquivo.grid(row=5, column=3, padx=10, pady=5)
+entry_nome_arquivo.grid(row=4, column=3, padx=10, pady=5)
 
 button_salvar = tk.Button(root, text="Salvar", command=salvar_dados)
-button_salvar.grid(row=7, column=1, pady=10)
+button_salvar.grid(row=5, column=1, pady=10)
 
 button_limpar = tk.Button(root, text="Limpar", command=limpar_campos)
-button_limpar.grid(row=7, column=2, pady=10)
+button_limpar.grid(row=5, column=2, pady=10)
 
 # Inicia a aplicação
 root.mainloop()
